a338349e3693d9ede3ea7a620903409f55f5ee3d
AbhiR4mRs <abhiramvellur40@gmail.com>
2025-04-19 23:56:17 +0530

    home page

--- sub_center/models.py
+++ sub_center/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Center(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_centers')  # HQ
+    # created_at, contact_number, etc. (optional)
